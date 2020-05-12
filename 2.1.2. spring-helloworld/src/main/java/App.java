f8cfaf3fe9859a6f40474a8b74e839bea50d450e
markitan1990 <maar201000@gmail.com>
2020-05-12 16:34:39 +0300

    Добавил решение 2.1.2 спринг

---
+++ 2.1.2. spring-helloworld/src/main/java/App.java
@@ -0,0 +1,23 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.w3c.dom.ls.LSOutput;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = applicationContext.getBean(Cat.class);
+        Cat cat2 = applicationContext.getBean(Cat.class);
+
+        System.out.println(bean.getMessage());
+
+        System.out.println(bean == bean2);
+        System.out.println(cat2 == cat);
+
+        
+    }
+}
